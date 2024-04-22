9d3928d62c941f630981de1ed5a6a4689ba62e7e
Emilie-Gilles <emilie-gilles@hotmail.com>
2024-04-22 17:05:06 +0200

    le bouton modifier pose problème

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,6 +7,8 @@ function TodoList() {
   const [tasks, setTasks] = useState([]);
   //dans notre cas, on crée un état local qui est une chaine de caractères vide
   const [newTask, setNewTask] = useState("");
+  //on initie un id à 0
+  const [id, setId] = useState(0);
 
   //fonction qui permet d'ajouter une tâche
   function addTask() {
@@ -14,29 +16,31 @@ function TodoList() {
     if (newTask === "") {
       return;
     }
-    //on ajoute la tâche au tableau et on lui donne un id
-    setTasks([...tasks, { id: tasks.length, name: newTask, checked: false }]);
+    //on ajoute la tâche au tableau et on lui donne un id qui sera toujours unique
+    setTasks([...tasks, { name: newTask, checked: false, id: id }]);
+    //on incrémente l'id
+    setId(id + 1);
     //on remet la tâche à vide
     setNewTask("");
   }
 
   //function pour si task.done est false, le bouton supprimer n'est pas cliquable
-  function removeTask(index) {
+  function removeTask(id) {
     const newTasks = [...tasks];
-    if (newTasks[index].checked === false) {
-      return;
-    }
+    const task = newTasks.find((task) => task.id === id);
+    // if (!task.checked) {
+    //   return;
+    // }
+    const index = newTasks.indexOf(task);
     newTasks.splice(index, 1);
     setTasks(newTasks);
   }
 
   //fonction qui permet de cocher une tâche
-  function checkTask(index) {
-    //on crée une copie du tableau
+  function checkTask(id) {
     const newTasks = [...tasks];
-    //on coche la tâche à l'index donné
-    newTasks[index].checked = !newTasks[index].checked;
-    //on met à jour le tableau
+    const task = newTasks.find((task) => task.id === id);
+    task.checked = !task.checked;
     setTasks(newTasks);
   }
 
@@ -50,16 +54,45 @@ function TodoList() {
     );
   }
 
-  //fonction pour modifier une tache en appuyant sur le bouton modifier le texte de la tache s'affiche dans une nouvelle input
-  function modifyTask(index) {
+  //fonction pour modifier une tache en appuyant sur le bouton modifier, le span qui a ka classe taskName devient un input
+
+  function modifyTask(id) {
+    //on recupère la tâche à modifier
     const newTasks = [...tasks];
-    const task = newTasks[index];
-    const newName = prompt("Modifier la tâche", task.name);
-    if (newName === null) {
-      return;
-    }
-    task.name = newName;
-    setTasks(newTasks);
+    const task = newTasks.find((task) => task.id === id);
+    //on recupère l'index de la tâche à modifier
+    const index = newTasks.indexOf(task);
+    //on crée un input
+    const input = document.createElement("input");
+    //on lui donne la valeur de la tâche à modifier
+    input.value = task.name;
+    //on recupere le span qui a la classe taskName et dont l'index est celui de la tâche à modifier
+    const span = document.querySelectorAll(".taskName")[index];
+    //on remplace le span par l'input
+    span.replaceWith(input);
+    //on recupère le bouton modifier qui a l'index de la tâche à modifier
+    const modifyBtn = input.nextElementSibling.children[0];
+    //on change le texte du bouton modifier en valider
+    modifyBtn.textContent = "Valider";
+    //on ajoute un event listener sur le bouton valider
+    modifyBtn.addEventListener("click", () => {
+      //si l'input est vide, on ne fait rien
+      if (input.value === "") {
+        return;
+      }
+      //on remplace l'input par un span
+      const newSpan = document.createElement("span");
+      newSpan.textContent = input.value;
+      input.replaceWith(newSpan);
+      //on remet le texte du bouton valider en modifier
+      modifyBtn.textContent = "Modifier";
+      //on modifie la tâche dans le tableau
+      task.name = input.value;
+      //on remplace la tâche dans le tableau
+      newTasks.splice(index, 1, task);
+      //on remet à jour le tableau
+      setTasks(newTasks);
+    });
   }
 
   //function pour afficher les taches à faire sous forme de liste si task.done est false
@@ -76,11 +109,16 @@ function TodoList() {
               <input
                 type="checkbox"
                 checked={task.checked}
-                onChange={() => checkTask(index)}
+                onChange={() => checkTask(task.id)}
               />
-              {task.name}
-              <button onClick={() => removeTask(index)}>Supprimer</button>
-              <button onClick={() => modifyTask(index)}>Modifier</button>
+              <span className="taskName">{task.name}</span>
+
+              <div className="allTasks__tasksToDo__btns">
+                <button onClick={() => modifyTask(task.id)}>Modifier</button>
+                <button id="deleteBtn" onClick={() => removeTask(task.id)}>
+                  Supprimer
+                </button>
+              </div>
             </li>
           ))}
         </ul>
@@ -98,8 +136,10 @@ function TodoList() {
         <ul>
           {doneTasks.map((task, index) => (
             <li key={index}>
-              {task.name}
-              <button onClick={() => removeTask(index)}>Supprimer</button>
+              <span className="taskName">{task.name}</span>
+              <button id="deleteBtn" onClick={() => removeTask(task.id)}>
+                Supprimer
+              </button>
             </li>
           ))}
         </ul>
@@ -113,6 +153,7 @@ function TodoList() {
         <input
           type="text"
           value={newTask}
+          placeholder="Ajouter une tâche"
           onChange={(event) => setNewTask(event.target.value)}
         />
         <button onClick={addTask}>Ajouter</button>
@@ -126,5 +167,11 @@ function TodoList() {
     </section>
   );
 }
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.querySelector("button").click();
+  }
+});
 
 export default TodoList;
